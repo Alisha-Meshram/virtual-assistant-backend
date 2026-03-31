78cc932a2cd490001c4d88ce695809ddd4e1ea04
Alisha-Meshram <aalishameshram15@gmail.com>
2026-03-31 09:40:25 +0530

    add a changes

--- index.js
+++ index.js
@@ -10,23 +10,9 @@ import { geminiResponse } from './gemini.js';
 dotenv.config()
 
 const app=express()
-// app.use(cors({origin:'http://localhost:5174',credentials:true}))
+app.use(cors())
 
-const allowedOrigins = [
-    "http://localhost:5173",
-    "https://virtual-assistant-phi-weld.vercel.app"
-  ];
-  
-  app.use(cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true
-  }));
+
 app.use(express.json())
 app.use(cookieParser())
 app.use('/api/v1',authRouter)
